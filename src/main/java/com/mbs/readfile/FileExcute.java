11ae85d328793f15af250572754a5745a21a97d8
sunmd <sunmd@sunmd-PC-DELL>
2016-05-31 11:21:21 +0800

    junit测试已经写完。下面进行处理界面

--- src/main/java/com/mbs/readfile/FileExcute.java
+++ src/main/java/com/mbs/readfile/FileExcute.java
@@ -19,128 +19,209 @@ import java.util.regex.Pattern;
 import org.apache.log4j.Logger;
 
 /**
- * 这个类主要对于文件进行处理
+ * 这个类主要对于文件进行处理，由于有对文件进行修改，所以这里制作成为单例模式
+ * 
  * @author sunmd
  *
  */
 public class FileExcute {
+
+	/**
+	 * 声明内部静态类，持有本对象引用行程，单例模式
+	 * 
+	 * @author sunmd
+	 *
+	 */
 	private static class LazyHolder {
 		private static final FileExcute INSTANCE = new FileExcute();
 	}
 
+	/**
+	 * 静态打印对象，用来记录日志
+	 */
 	private static Logger logger = Logger.getLogger(FileExcute.class);
-	
-	
+
+	/**
+	 * 私有化内部初始化方法
+	 */
 	private FileExcute() {
+		super();
 	}
 
+	/**
+	 * 获得单向里对象
+	 * 
+	 * @return 返回单例的FileExcute对象
+	 */
 	public static FileExcute getInstance() {
 		return LazyHolder.INSTANCE;
 	}
 
-	public List<String> getFile(String path) {
-		logger.debug("this is getFile");
-		File file = new File(path);
-		FileInputStream fis = null;
-		BufferedReader br = null;
+	/**
+	 * 获取文件里面的数据信息，以行为单元，一行一个数据
+	 * 
+	 * @param file
+	 *            可以读的数据文件，一行记录一个文件名称
+	 * @return 返回List<String>将数据已list传递，没有数据或者不合法将返回null
+	 */
+	public List<String> readFileList(File file) {
+
 		List<String> list = null;
-		logger.debug("exists = " + file.exists() + "canRead = "
-				+ file.canRead());
+
+		FileInputStream fis = null;
+		InputStreamReader isr = null;
+		BufferedReader br = null;
+		boolean exists = file.exists();//是否存在文件的标识符
+		boolean canRead = file.canRead();//是否能去数据标识符
+		
+		logger.debug("readFileList exists = " + exists + "&canRead = " + canRead);
+		
 		if (file.exists() && file.canRead()) {
 			try {
 				fis = new FileInputStream(file);
-				br = new BufferedReader(new InputStreamReader(fis));
+				isr = new InputStreamReader(fis);
+				br = new BufferedReader(isr);
 				list = new ArrayList<String>();
 				String str = br.readLine();
 				while (str != null) {
 					list.add(str);
-					logger.debug("this is str = " + str);
+					logger.debug("readFileList readLine str = " + str);
 					str = br.readLine();
 				}
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
+				logger.error("readFileList FileNotFoundException ");
 			} catch (IOException e) {
 				e.printStackTrace();
+				logger.error("readFileList IOException ");
 			} finally {
 				try {
 					br.close();
+					isr.close();
 					fis.close();
-
 				} catch (IOException e) {
 					e.printStackTrace();
+					logger.error("readFileList IOException ");
+				} finally {
+					br = null;
+					isr = null;
+					fis = null;
 				}
 			}
+
 		}
 		return list;
 	}
-
-	public void delete(String path, List<String> list) {
+	
+	/**
+	 * 获取文件里面的数据信息，以行为单元，一行一个数据
+	 * @param path 数据文件路径
+	 * @return 返回List<String>将数据已list传递，没有数据或者不合法将返回null
+	 */
+	public List<String> readFileList(String path) {
+		logger.debug("getFile is beagin");
 		File file = new File(path);
+		List<String> list = readFileList(file);
+		logger.debug("getFile is end");
+		return list;
+	}
+	
+	/**
+	 * 内部类filter，只接受后缀txt文件
+	 * @author sunmd
+	 *
+	 */
+	private class TextFilter implements FilenameFilter{
+
+		public boolean accept(File dir, String name) {
+			logger.debug("TextFilter accept name = " + name);
+			Pattern pattern = Pattern.compile("^.*.txt$");
+			Matcher matcher = pattern.matcher(name);
+			logger.debug("TextFilter matches = " + matcher.matches());
+			return matcher.matches();
+		}
+		
+	}
+	
+	/**
+	 * 将list里面不包含的txt文件删除
+	 * @param deletePath 需要删除的对应路径
+	 * @param list 删除的白名单
+	 */
+	public void delete(String deletePath, List<String> list) {
+		logger.debug("delete deletePath = " + deletePath);
+		File file = new File(deletePath);
 		if (file.exists() && file.isDirectory()) {
 			String str = "";
-			File[] subFiles = file.listFiles(new FilenameFilter() {
-
-				public boolean accept(File dir, String name) {
-					logger.debug("name = " + name);
-					Pattern pattern = Pattern.compile("^.*.txt$");
-					Matcher matcher = pattern.matcher(name);
-					// [^\\]*\\.txt$
-					logger.debug("1111 = " + matcher.matches());
-					return matcher.matches();
-				}
-			});
+			
+			File[] subFiles = file.listFiles(new TextFilter());
+			//对于搜索到的文件进行循环查看
 			for (int i = 0; i < subFiles.length; i++) {
 				boolean flag = false;
 				str = subFiles[i].getAbsolutePath();
+				
 				for (int j = 0; j < list.size(); j++) {
-
 					if (str.indexOf(list.get(j)) >= 0) {
+						logger.debug("str = " + str + "&j=" + list.get(j));
 						flag = true;
 					}
 				}
-				if (!flag) {
+				
+				if (!flag) {//如果没有搜索到就进行删除
+					logger.debug("delete deleteFile = " + str);
 					subFiles[i].delete();
 				}
 			}
 		}
 	}
 
-	public void writeText(String path1, String path2) {
-		File file = new File(path1);
-		File[] subFiles = file.listFiles(new FilenameFilter() {
+	/**
+	 * 根据文件夹里面的txt文件写入文件中
+	 * @param FilePath 获取的文件路径
+	 * @param WritePath 写入文件路径
+	 */
+	public void writeText(String filePath, String writePath) {
+		logger.debug("writeText filePath = " + filePath + "&writePath = " + writePath);
+		File file = new File(filePath);
+		File[] subFiles = file.listFiles(new TextFilter());
 
-			public boolean accept(File dir, String name) {
-				logger.debug("name = " + name);
-				Pattern pattern = Pattern.compile("^.*.txt$");
-				Matcher matcher = pattern.matcher(name);
-				logger.debug("1111 = " + matcher.matches());
-				return matcher.matches();
-			}
-		});
-
-		OutputStream os;
+		OutputStream os = null;
 		BufferedWriter buffWrite = null;
+		OutputStreamWriter osw = null;
+		
 		try {
-			os = new FileOutputStream(new File(path2));
-			buffWrite = new BufferedWriter(new OutputStreamWriter(os));
+			
+			os = new FileOutputStream(new File(writePath));
+			osw = new OutputStreamWriter(os);
+			buffWrite = new BufferedWriter(osw);
+			String temp = "";
 			for (int i = 0; i < subFiles.length; i++) {
-				buffWrite.write(subFiles[i].getName());
+				//进行循环写入状态
+				temp = subFiles[i].getName();
+				logger.debug("writeText fileName = " + temp);
+				buffWrite.write(temp);
 				buffWrite.newLine();
 				buffWrite.flush();
 			}
 
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			logger.debug("writeText FileNotFoundException");
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			logger.debug("writeText IOException");
 		} finally {
 			try {
 				buffWrite.close();
+				osw.close();
+				os.close();
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
+				logger.debug("writeText IOException");
 				e.printStackTrace();
+			} finally {
+				buffWrite = null;
+				osw = null;
+				os = null;
 			}
 		}
 
--- src/test/java/com/mbs/readfile/FileExcuteTest.java
+++ src/test/java/com/mbs/readfile/FileExcuteTest.java
@@ -3,40 +3,104 @@ package com.mbs.readfile;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 @FixMethodOrder(MethodSorters.DEFAULT)
-public class FileExcuteTest {
+public class FileExcuteTest  {
 private static Logger logger = Logger.getLogger(FileExcuteTest.class);
+private static final String TESTPATH = ".\\src\\test\\resoures\\";
+private static final String TESTFILEBAK = "sdw2w";
+private static final String TESTFILE = "i5kof-95";
+private static final int LEN = 10;
+private List<String> list = null;
+	@BeforeClass
+	public static void testBeforeTest() {
+		logger.debug("testBeforeTest - beagin");
+		File file = null;
+		for(int i=0; i<LEN ;i++) {
+			file = new File(TESTPATH + TESTFILEBAK + i + ".txt");
+			logger.debug("testBeforeTest - name = " + TESTPATH + TESTFILEBAK + i + ".txt");
+				try {
+					file.createNewFile();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+	}
 	@Test
-	public void testReadFile() {
-		File test = new File(".");
-		logger.debug(test.getAbsolutePath());
-		FileExcute fe = FileExcute.getInstance();
-		List<String> list = fe.getFile(".\\src\\test\\resoures\\test.txt");
-		assertFalse(list.isEmpty());
+	public void testWriteFile() {
+		
+		FileExcute.getInstance().writeText(TESTPATH, TESTPATH + TESTFILEBAK + "0.txt");
+		list = FileExcute.getInstance().readFileList(TESTPATH + TESTFILEBAK + "0.txt");
+		logger.debug("testWriteFile - list = " + list.size());
+		assertNotNull(list);
 		
 	}
 	
 	@Test
-	public void testFind() {
+	public void testReadFile() {
 		FileExcute fe = FileExcute.getInstance();
-		List<String> list = fe.getFile(".\\src\\test\\resoures\\test.txt");
-		String path = ".\\src\\test\\resoures\\test";
-		fe.delete(path, list);
+		logger.debug("123131212=====" + TESTPATH + TESTFILEBAK + "0.txt");
+		File file = new File(TESTPATH + TESTFILEBAK + "0.txt");
+		logger.debug("123131212=====" + file.exists());
+		List<String> list1 = fe.readFileList(file);
+		assertNotNull(list1);
 	}
 	
-	@Test
-	public void testWriteFile() {
-		FileExcute fe = FileExcute.getInstance();
-		String path1 = ".\\src\\test\\resoures\\test";
-		String path2 = ".\\src\\test\\resoures\\test2.txt";
-		fe.writeText(path1,path2);
+	@Test(timeout=100)
+	public void testDelete() {
+		logger.debug("testDelete - beagin");
+		File file = null;
+		for(int i=0; i<LEN ;i++) {
+			file = new File(TESTPATH + TESTFILE + i + ".txt");
+				try {
+					file.createNewFile();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+		
+		logger.debug("testDelete - 123456 list = " + (list ==null));
+		list = FileExcute.getInstance().readFileList(TESTPATH + TESTFILEBAK + "0.txt");
+		logger.debug("testDelete - 1234567 list = " + (list ==null));
+		
+		FileExcute.getInstance().delete(TESTPATH, list);
+		file = new File(TESTPATH);
+		File[] files = file.listFiles();
+		for(int i=0; i< files.length; i++) {
+			logger.debug("testDelete - getName = " + files[i].getName());
+			if(files[i].getName().indexOf(TESTFILE) >= 0) {
+				fail("this delete not clear");
+			}
+		}
 	}
+	
+	@AfterClass
+	public static void testAfterTest() {
+		File file = null;
+		for(int i=0; i<LEN ;i++) {
+			file = new File(TESTPATH + TESTFILEBAK + i + ".txt");
+				if(file.exists()) {
+					file.delete();
+				}
+		}
+	}
+
+
+//	@Test
+//	public void testDemo() {
+//		FileExcute fe = FileExcute.getInstance();
+//		List<String> list = fe.readFileList(".\\src\\test\\resoures\\test2.txt");
+//		String path = "F:\\WorkSpace\\readfile\\src\\test\\resoures\\txt";
+//		fe.delete(path, list);
+//	}
 
 }
